52d57e57576d1ea7579e2804d7e9170c00f3edb2
Roslyn Harris <lilmsrosie87@yahoo.com>
2017-05-04 21:51:23 -0700

    added greeting in js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
-function myFunction() {
-    alert("I am an alert box!");
+var today = new Date();
+var hourNow = today.getHours();
+var greeting; 
+
+if (hourNow > 18) {
+	greeting = 'Good evening!';
+} else if (hourNow > 12) {
+	greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+	greeting = 'Good morning!';
+} else{
+	greeting = 'Welcome';
 }
 
-
+document.write('<h1>' + greeting + '</h1>');
